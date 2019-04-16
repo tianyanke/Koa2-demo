a388e16abb61b8701f90b7dfa519612888ab6158
HoChine <HoChiner@163.com>
2019-04-16 17:50:16 +0800

    koa2学习笔记02 - 给koa2添加系统日志 —— node日志管理模块log4js

--- app.js
+++ app.js
@@ -22,15 +22,23 @@ app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   // extension: 'pug'
-  extension: 'html'
+  extension: 'html'   //html 后缀的模板
 }))
 
+
 // logger
+const logsUtil = require('./utils/logs.js');
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+    const start = new Date();					// 响应开始时间
+    let intervals;								// 响应间隔时间
+    try {
+        await next();
+        intervals = new Date() - start;
+        logsUtil.logResponse(ctx, intervals);	//记录响应日志
+    } catch (error) {
+        intervals = new Date() - start;
+        logsUtil.logError(ctx, error, intervals);//记录异常日志
+    }
 })
 
 // routes
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
 const router = require('koa-router')()
 
+const logsHandle = require("../utils/logs.js").logHandle;
+const logsInfo = require("../utils/logs.js").logInfo;
+
+
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+	await ctx.render('index', {
+		title: 'Hello Koa 2!'
+	})
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+	ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+	logsHandle("请求了json接口")  // 假设此处需要记录敏感操作的日志
+	logsInfo("请求了json接口")    // 假设此处需要在控制台打印日志
+	ctx.body = {
+		title: 'koa2 json'
+	}
 })
 
 module.exports = router
